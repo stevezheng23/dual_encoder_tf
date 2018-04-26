a251c250ed140cdfb7c5be1362724526beb214aa
Mingzhi Zheng <mizheng@microsoft.com>
2018-04-26 21:23:50 +0800

    add default train method loading dynamic config

--- dual_encoder/dual_encoder_run.py
+++ dual_encoder/dual_encoder_run.py
@@ -14,7 +14,66 @@ def add_arguments(parser):
 
 def train(logger,
           hyperparams):
-    pass
+    logger.log_print("##### create train model #####")
+    train_model = create_train_model(logger, hyperparams)
+    logger.log_print("##### create eval model #####")
+    eval_model = create_eval_model(logger, hyperparams)
+    logger.log_print("##### create infer model #####")
+    infer_model = create_infer_model(logger, hyperparams)
+    
+    config_proto = get_config_proto(hyperparams["device"]["log_device_placement"],
+        hyperparams["device"]["allow_soft_placement"], hyperparams["device"]["allow_growth"],
+        hyperparams["device"]["per_process_gpu_memory_fraction"])
+    
+    train_sess = tf.Session(config=config_proto, graph=train_model.graph)
+    eval_sess = tf.Session(config=config_proto, graph=eval_model.graph)
+    infer_sess = tf.Session(config=config_proto, graph=infer_model.graph)
+    
+    logger.log_print("##### start training #####")
+    summary_output_dir = hyperparams["train"]["summary_output_dir"]
+    if not tf.gfile.Exists(summary_output_dir):
+        tf.gfile.MakeDirs(summary_output_dir)
+    
+    train_summary_writer = SummaryWriter(train_model.graph, os.path.join(summary_output_dir, "train"))
+    eval_summary_writer = SummaryWriter(eval_model.graph, os.path.join(summary_output_dir, "eval"))
+    infer_summary_writer = SummaryWriter(infer_model.graph, os.path.join(summary_output_dir, "infer"))
+    
+    init_model(train_sess, train_model)
+    init_model(eval_sess, eval_model)
+    init_model(infer_sess, infer_model)
+    
+    global_step = 0
+    train_model.model.save(train_sess, global_step)
+    train_logger = TrainLogger(hyperparams["data"]["log_output_dir"])
+    eval_logger = EvalLogger(hyperparams["data"]["log_output_dir"])
+    for epoch in range(hyperparams["train"]["num_epoch"]):
+        train_sess.run(train_model.data_pipeline.initializer)
+        step_in_epoch = 0
+        while True:
+            try:
+                start_time = time.time()
+                train_result = train_model.model.train(train_sess,
+                    train_model.src_embedding, train_model.trg_embedding)
+                end_time = time.time()
+                
+                global_step = train_result.global_step
+                step_in_epoch += 1
+                train_logger.update(train_result, epoch, step_in_epoch, end_time-start_time)
+
+                if step_in_epoch % hyperparamshyperparams["train"]["step_per_stat"] == 0:
+                    train_logger.check()
+                    train_summary_writer.add_summary(train_result.summary, global_step)
+                if step_in_epoch % hyperparamshyperparams["train"]["step_per_ckpt"] == 0:
+                    train_model.model.save(train_sess, global_step)
+            except tf.errors.OutOfRangeError:
+                train_logger.check()
+                train_model.model.save(train_sess, global_step)
+                break
+
+    train_summary_writer.close_writer()
+    eval_summary_writer.close_writer()
+    infer_summary_writer.close_writer()
+    logger.log_print("##### finish training #####")
 
 def evaluate(logger,
              hyperparams):
